f83aae4e14afcebcf026acdb3b5916e89e18532a
贵志 <blankList@outlook.com>
2018-11-21 10:44:37 +0800

    update
    console.log

--- src/index.js
+++ src/index.js
@@ -21,6 +21,7 @@ class App extends React.Component {
       this.setState({ count: this.state.count + 1 });
       console.log(this.state.count);
     }, 0);
+    console.log(this.state.count);
   }
 
   render() {
